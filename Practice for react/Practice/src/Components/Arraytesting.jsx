a0512b8bfe190d5352a14f1328e977f7c0edd8c1
SamipMaharjan <adsrb15@gmail.com>
2023-03-14 09:47:51 +0545

    just practicing useReducer

--- Practice for react/Practice/src/Components/Arraytesting.jsx
+++ Practice for react/Practice/src/Components/Arraytesting.jsx
@@ -1,35 +1,39 @@
-import React,{useState} from 'react'
+import React,{useState, useReducer} from 'react'
+
+
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+}
 
 export default function Arraytesting() {
-    const [items, setItems] = useState([]);
-    const tryingSomething = [{
-        id: 3,
-        value: 55
-    },
+  function reducer(state, action){
+    switch(action.type)
     {
-        id: 8,
-        value: 34
-    }
-];
-    const addItem = () => {
-      console.log("Retarded codes");
-        setItems([...items,{
-            id: items.length,
-            value: Math.floor(Math.random() * 10) + 1 
-        }])
+      case ACTIONS.INCREMENT:
+        return { count: state.count + 1 }
 
-        console.log(items[0]);
+      case ACTIONS.DECREMENT:
+        return { count: state.count - 1 }
+      
+      default:
+        alert("Error"); 
     }
+  }
+  const [state, dispatch] = useReducer(reducer, { count: 0 }) 
+
+  const increment = () => {
+    dispatch({type: ACTIONS.INCREMENT});
+  }
+  const decrement = () => {
+   dispatch({type: ACTIONS.DECREMENT});
+  }
+
   return (
-    <div>
-        <button onClick={addItem}>Add a number</button>
-      <ul>
-        {
-            items.map(item => (
-            <li key="item.id"><input type="checkbox" key="item.id"/>{item.value}</li>
-            ))
-        }
-      </ul>
-    </div>
+    <>
+     <button onClick={decrement}>-</button>
+     {state.count} 
+     <button onClick={increment}>+</button> 
+    </>
   )
 }
